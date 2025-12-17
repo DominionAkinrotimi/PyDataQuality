b9603df8a9d2f0a59c48bd5b8ced7855fbee247c
Dominion Akinrotimi <contact.dominionakinrotimi@gmail.com>
2025-12-17 15:33:33 +0100

    Bump version to 0.1.2

--- pydataquality/__init__.py
+++ pydataquality/__init__.py
@@ -2,7 +2,7 @@
 PyDataQuality - A comprehensive data quality analysis tool for Python.
 """
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __author__ = 'Dominion Akinrotimi'
 
 from .analyzer import DataQualityAnalyzer

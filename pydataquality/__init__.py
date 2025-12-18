f89f61088376a14b98a8e175ff1fba1b32292f05
Dominion Akinrotimi <contact.dominionakinrotimi@gmail.com>
2025-12-18 03:47:43 +0100

    Update __init__.py

--- pydataquality/__init__.py
+++ pydataquality/__init__.py
@@ -2,7 +2,7 @@
 PyDataQuality - A comprehensive data quality analysis tool for Python.
 """
 
-__version__ = '0.1.7'
+__version__ = '0.1.0'
 __author__ = 'Dominion Akinrotimi'
 
 from .analyzer import DataQualityAnalyzer

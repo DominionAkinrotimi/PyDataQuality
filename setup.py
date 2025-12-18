897f1c2aa28f98564919a016ce3bff05082d13bb
Dominion Akinrotimi <contact.dominionakinrotimi@gmail.com>
2025-12-18 03:42:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pydataquality",
-    version="0.1.6",
+    version="0.1.7",
     author="Dominion Akinrotimi",
     author_email="contact.dominionakinrotimi@gmail.com",
     description="Enterprise-grade automated data quality assessment and reporting engine for pandas",
@@ -60,6 +60,7 @@ setup(
     },
 
 )
+
 
 
 
